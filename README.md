6a7e8c2167ac86e9637db7c4fc9653c3dfb3d775
Niamh Brockbank <niamh.brockbank@openbet.com>
2024-11-29 11:57:33 +0000

    Render selectable questions

---
+++ README.md
@@ -0,0 +1,1 @@
+Note that in the `questions.js` file the correct answer is always the first one in the array.
---
+++ src/component/Quiz.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import QUESTIONS from "../questions";
+
+export default function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+  const activeQuestion = QUESTIONS[activeQuestionIndex];
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{activeQuestion.text}</h2>
+        <ul id="answers">
+          {activeQuestion.answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
